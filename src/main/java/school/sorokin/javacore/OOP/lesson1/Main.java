0a5076703340d4806aa09aa6bc6294199518ab72
DmitryB1970 <badl1970@mai.ru>
2025-09-06 22:10:55 +0300

    homework OOP lesson 1 is done

---
+++ src/main/java/school/sorokin/javacore/OOP/lesson1/Main.java
@@ -0,0 +1,11 @@
+package school.sorokin.javacore.OOP.lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.setTitle("Война и мир");
+        book.setAuthor("Лев Толстой");
+        book.read();
+    }
+}
