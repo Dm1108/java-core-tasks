92faf806330e95c01becd1835c4cb9de0bea98a1
DmitryB1970 <badl1970@mai.ru>
2025-09-07 00:25:36 +0300

    homework OOP lesson 2 is done

---
+++ src/main/java/school/sorokin/javacore/OOP/lesson2/Car.java
@@ -0,0 +1,16 @@
+package school.sorokin.javacore.OOP.lesson2;
+
+public class Car {
+
+    String model;
+    String color;
+    int wheelCount;
+    double weight;
+    int maxSpeed;
+
+
+    protected void finalize() {
+
+        System.out.println("Car destroyed");
+    }
+}
