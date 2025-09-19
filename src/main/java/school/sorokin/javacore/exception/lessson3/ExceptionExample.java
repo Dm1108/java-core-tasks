d44908afb321d19c6a57171bb6d6752387266134
Dm1108 <bar.dm.leo@gmail.com>
2025-09-19 19:34:31 +0300

    Module Exception lesson 3 is done

---
+++ src/main/java/school/sorokin/javacore/exception/lessson3/ExceptionExample.java
@@ -0,0 +1,64 @@
+package school.sorokin.javacore.exception.lessson3;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExceptionExample {
+
+    private static final Logger logger = Logger.getLogger(ExceptionExample.class.getName());
+    private static final Scanner scanner = new Scanner(System.in);
+    private static List<String> operationInfo = new ArrayList<>();
+    private static int number1;
+    private static int number2;
+
+
+    public static void main(String[] args) {
+
+        while (true) {
+            try {
+                System.out.println("Введите первое число: ");
+                number1 = scanner.nextInt();
+                System.out.println("Введите второе число: ");
+                number2 = scanner.nextInt();
+                scanner.nextLine();
+                int result = getDivision(number1, number2);
+                String info = "Результат деления: " + number1 + " / " + number2 + " = " + result;
+                logger.log(Level.INFO, info);
+                operationInfo.add(info);
+            } catch (InputMismatchException e) {
+                String mismatch = "Вы ввели не число. Повторите ввод";
+                logger.log(Level.SEVERE, mismatch);
+                operationInfo.add(mismatch);
+            } catch (ArithmeticException e) {
+                String arithmetic = "Деление " + number1 + " / " + number2 + " завершилось ошибкой: деление на ноль";
+                logger.log(Level.SEVERE, arithmetic);
+                operationInfo.add(arithmetic);
+            }
+            System.out.println("Хотите выполнить ещё одно деление: да / нет ");
+            String yesOrNo = scanner.nextLine();
+            while (true) {
+                if (yesOrNo.equalsIgnoreCase("да") || yesOrNo.equalsIgnoreCase("нет")) {
+                    switch (yesOrNo) {
+                        case "да":
+                            break;
+                        case "нет":
+                            System.out.println(operationInfo);
+                            System.exit(0);
+                    }
+                } else {
+                    System.out.println("Вы ввели неправильное слово. Повторите ввод");
+                    scanner.nextLine();
+                }
+                break;
+            }
+        }
+    }
+
+    private static int getDivision(int number1, int number2) {
+        return number1 / number2;
+    }
+}
